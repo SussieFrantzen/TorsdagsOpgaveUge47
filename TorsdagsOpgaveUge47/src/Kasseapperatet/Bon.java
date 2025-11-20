284fd6d9b9bc068b7f8cec9127acbdbc6474f070
SussieFrantzen <cph-sf252@stud.ek.dk>
2025-11-20 22:33:55 +0100

    ...
    ...

--- TorsdagsOpgaveUge47/src/Kasseapperatet/Bon.java
+++ TorsdagsOpgaveUge47/src/Kasseapperatet/Bon.java
@@ -5,13 +5,13 @@ import Varer.ScanningSystem;
 import Varer.Vare;
 
 public class Bon {
-    //Printer varedeklaration, pris og tilbudspris
+    //Printer varedeklaration, pris
     ScanningSystem scanningSystem = new ScanningSystem();
     Vare vare = new Vare();
     Kurv kurv = new Kurv();
     public void bon() {
         System.out.println("----- VAREINFO -----");
-        for (Vare vare : kurv.getKurv()) {     // vi laver getKurvListe() i Kurv klassen
+        for (Vare vare : kurv.getKurv()) {
             System.out.println("Vare: " + vare.getVarerDeklaration());
             System.out.println("Pris: " + vare.getPris());
             System.out.println();
